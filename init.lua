48ea8eab4b79475bb8ad9c542b42625c4967ff28
Noah Spielman <nspielman@live.com>
2024-01-25 18:53:55 -0700

    making it my own

--- init.lua
+++ init.lua
@@ -1,2 +1,14 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+require("plugins.kanban")
+-- require("telescope")
+-- require("telescope").load_extension("kanban")
+
+vim.cmd("colorscheme melange")
+vim.cmd("highlight Normal guibg=NONE")
+vim.cmd("highlight NeoTreeNormal guibg=NONE")
+vim.cmd("highlight NvimTreeNormal guibg=NONE")
+
+-- set key to toggle kanban
+vim.api.nvim_set_keymap("n", "<Leader>kb", ":Kanban<CR>", { noremap = true, silent = true })
